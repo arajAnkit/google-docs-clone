063345258c3b32d342db91d405d99d537f1c2849
Ankit Rajpoot <marketing.araj@gmail.com>
2024-12-20 18:06:17 +0530

    pt15 : Template Gallery

---
+++ src/constants/templates.ts
@@ -0,0 +1,21 @@
+export const templates = [
+  { id: "blank", lebel: "Blank Document", imageUrl: "/blank-document.svg" },
+  {
+    id: "software-proposal",
+    lebel: "Software development proposal",
+    imageUrl: "/software-proposal.svg",
+  },
+  {
+    id: "project-proposal",
+    lebel: "Project proposal",
+    imageUrl: "/project-proposal.svg",
+  },
+  {
+    id: "business-letter",
+    lebel: "Business letter",
+    imageUrl: "/business-letter.svg",
+  },
+  { id: "resume", lebel: "Resume", imageUrl: "/resume.svg" },
+  { id: "cover-letter", lebel: "Cover letter", imageUrl: "/cover-letter.svg" },
+  { id: "letter", lebel: "Letter", imageUrl: "/letter.svg" },
+];
