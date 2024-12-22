5dcae8f24ac4ca24585dbb13880637d66b43f900
Ankit Rajpoot <marketing.araj@gmail.com>
2024-12-22 10:25:41 +0530

    pt19 : Document remove, rename and paginaton functionality

--- src/app/(home)/documents-table.tsx
+++ src/app/(home)/documents-table.tsx
@@ -9,6 +9,8 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+
 import { LoaderIcon } from "lucide-react";
 import { DocumentRow } from "./document-row";
 
@@ -59,6 +61,16 @@ export const DocumentsTable = ({
           )}
         </Table>
       )}
+      <div className="flex items-center justify-center ">
+        <Button
+          variant="ghost"
+          size="sm"
+          onClick={() => loadMore(5)}
+          disabled={status !== "CanLoadMore"}
+        >
+          {status === "CanLoadMore" ? "Load more" : "End of results"}
+        </Button>
+      </div>
     </div>
   );
 };
--- src/components/colorful-ripple-loader.tsx
+++ src/components/colorful-ripple-loader.tsx
@@ -6,7 +6,7 @@ export const ColorfulRippleLoader = ({ label }: ColorfulRippleLoaderProps) => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center gap-8">
       <div className="relative flex items-center justify-center">
-        <div className="absolute h-12 w-12 rounded-full border-t-4 border-blue-400 animate-spin-slow" />
+        <div className="absolute h-12 w-12 rounded-full border-t-4 border-blue-400 animate-spin-slows" />
         <div className="absolute h-12 w-12 rounded-full border-t-4 border-green-400 animate-spin-slower" />
         <div className="absolute h-12 w-12 rounded-full border-t-4 border-red-400 animate-spin-slowest" />
       </div>
